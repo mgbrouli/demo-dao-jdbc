cf22547df2e906e2d3654ab27b8c3d621497df69
Welligton Clemente <wtnbrouli@gmail.com>
2026-01-18 21:27:42 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import org.example.models.entities.Department;
+import org.example.models.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,7 +11,9 @@ public class Main {
     static void main() {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
