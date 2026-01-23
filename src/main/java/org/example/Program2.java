ee1124cfbde66278b5bf1f41872df9d6e0c763a1
Welligton Clemente <wtnbrouli@gmail.com>
2026-01-22 21:44:04 -0300

    Implements deletByID

--- src/main/java/org/example/Program2.java
+++ src/main/java/org/example/Program2.java
@@ -13,12 +13,19 @@ public class Program2 {
 
         System.out.println("\n=== TEST 1: Department INSERT ===");
         Department newDepartment = new Department(null, "Musica");
-        departmentDao.insert(newDepartment);
+        //departmentDao.insert(newDepartment);
         System.out.println("Inserted! new id = "+ newDepartment.getId());
 
         System.out.println("\n=== TEST 2: Department UPDATE ===");
         int id = 7;
         departmentDao.update(new Department(id, "Pantera"));
         System.out.println("Updated obj "+ id);
+
+        System.out.println("\n=== TEST 3: Department DELETE ===");
+        departmentDao.deleteById(9);
+        departmentDao.deleteById(10);
+        departmentDao.deleteById(11);
+        System.out.println("Delete for id's 9, 10 e 11");
+
     }
 }
